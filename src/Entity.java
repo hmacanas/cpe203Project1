fe3215223fc215f461abc835bde4286ae1f5f2cf
Henry Macanas <henry@Henrys-MacBook-Pro.local>
2019-05-27 22:34:18 -0700

    removed nextPositionOreblob from Entity

--- src/Entity.java
+++ src/Entity.java
@@ -33,30 +33,4 @@ public abstract class Entity
     public String getId() {
         return id;
     }
-
-    public Point nextPositionOreBlob(WorldModel world,
-                                     Point destPos)
-    {
-        int horiz = Integer.signum(destPos.x - this.position.x);
-        Point newPos = new Point(this.position.x + horiz,
-                this.position.y);
-
-        Optional<Entity> occupant = world.getOccupant(newPos);
-
-        if (horiz == 0 ||
-                (occupant.isPresent() && !(occupant.get().getClass() == Ore.class)))
-        {
-            int vert = Integer.signum(destPos.y - this.position.y);
-            newPos = new Point(this.position.x, this.position.y + vert);
-            occupant = world.getOccupant(newPos);
-
-            if (vert == 0 ||
-                    (occupant.isPresent() && !(occupant.get().getClass() == Ore.class)))
-            {
-                newPos = this.position;
-            }
-        }
-
-        return newPos;
-    }
 }
